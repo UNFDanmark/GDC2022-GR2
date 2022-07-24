bc6f7273f6a6edfb04d649dbf7ed3036d66fd98b
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-24 15:27:43 +0200

    Improved Star Spawner
    Improved Star Spawner

--- GDC-project/Assets/Scripts/PlayerScript.cs
+++ GDC-project/Assets/Scripts/PlayerScript.cs
@@ -7,15 +7,19 @@ public class PlayerScript : MonoBehaviour
     public float startJumpForce = 500f;
     public float moveSpeed = 5f;
     public float jumpModifier = 1f;
-    public float HKForce = 1f;
+    //public float HKForce = 1f;
+    public float startAttackSpeed = 1f;
+    public float weakJump = 1f;
 
     Vector3 previousVel;
     Vector3 previousPos;
 
     bool firstJump = true;
+    bool starHit = false;
+    public bool isAttacking = false;
 
-    //if hit from above with attack, jump high
-    //if hit from above without attack lil jump (enough to try again)
+    //if hit from above with attack, jump high, destroy star
+    //if hit from above without attack lil jump (enough to try again), dont destroy star
     //if hit from below, fall down (die?)
     //the faster the attack/dash, the more powerful the jump boost
 
@@ -30,8 +34,11 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Save current speed and position
         previousVel = rb.velocity;
         previousPos = transform.position;
+
+        //Move and attack
         MoveHandler();
         AttackHandler();
     }
@@ -42,7 +49,9 @@ public class PlayerScript : MonoBehaviour
 
         rb.velocity = new Vector3(moveInput * moveSpeed, rb.velocity.y, 0);
 
-        if(firstJump && Input.GetButtonDown("Jump"))
+        bool wantsToJump = Input.GetButtonDown("Jump");
+
+        if (firstJump && wantsToJump && !starHit)
         {
             Debug.Log("Jump dangit");
             rb.AddForce(Vector3.up * startJumpForce);
@@ -52,26 +61,48 @@ public class PlayerScript : MonoBehaviour
 
     void AttackHandler()
     {
-
+        if (Input.GetKeyDown("s"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, -startAttackSpeed, rb.velocity.z); //Sets the vertical speed to attack speed
+            isAttacking = true;
+        }
+        else if (Input.GetKeyUp("s"))
+        {
+            isAttacking = false;
+        }
     }
 
     
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.tag == "StarTop")
         {
-            firstJump = true;
-        }
-        else if (other.gameObject.tag == "StarTop")
-        {
-            Debug.Log("Jump from star!");
-            transform.position = previousPos;
-            rb.velocity = new Vector3(previousVel.x, -previousVel.y * jumpModifier, previousVel.z);
-            Destroy(other.transform.parent.gameObject);
+            if(isAttacking)
+            {
+                print("attack!");
+                transform.position = previousPos;
+                rb.velocity = new Vector3(previousVel.x, -previousVel.y * jumpModifier, previousVel.z);
+                Destroy(other.transform.parent.gameObject);
+            }
+            else
+            {
+                transform.position = previousPos;
+                rb.velocity = new Vector3(previousVel.x, weakJump, previousVel.z);
+            }
+            isAttacking=false;
+            starHit = true;
         }
         else if (other.gameObject.tag == "StarBottom")
         {
             Debug.Log("U suck");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            firstJump = true;
         }
     }
 }
--- GDC-project/Assets/Scripts/StarSpawner.cs
+++ GDC-project/Assets/Scripts/StarSpawner.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class StarSpawner : MonoBehaviour
 {
     public float width = 10f;
+    public float startSpawnHeight = 10f;
+    public float startSpawnHeightOffset;
 
     float currentSpawnDelay;
     public float minSpawnDelay = 0.5f;
     public float maxSpawnDelay = 1.5f;
+
+    public int startStarAmount = 4;
 
     float lastStarSpawn = 0f;
 
@@ -17,7 +21,12 @@ public class StarSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnStar();
+        for (int i = 0; i < startStarAmount; i++)
+        {
+            float minY = transform.position.y + startSpawnHeightOffset - startSpawnHeight / 2;
+            float maxY = transform.position.y + startSpawnHeightOffset + startSpawnHeight / 2;
+            SpawnStar(Random.Range(minY, maxY));
+        }
     }
 
     // Update is called once per frame
@@ -25,17 +34,17 @@ public class StarSpawner : MonoBehaviour
     {
         if(Time.time - lastStarSpawn >= currentSpawnDelay)
         {
-            SpawnStar();
+            SpawnStar(transform.position.y);
             lastStarSpawn = Time.time;
 
             currentSpawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
         }
     }
 
-    void SpawnStar()
+    void SpawnStar(float yPos)
     {
         float xPos = Random.Range(transform.position.x - width / 2, transform.position.x + width / 2);
-        Vector3 spawnPos = new Vector3(xPos ,transform.position.y, transform.position.z);
+        Vector3 spawnPos = new Vector3(xPos , yPos, transform.position.z);
         Instantiate(prefab, spawnPos, Quaternion.identity);
     }
 
@@ -43,5 +52,9 @@ public class StarSpawner : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube(transform.position, new Vector3(width, 1, 1));
+        Gizmos.color = Color.green;
+        Vector3 boxPos = new Vector3(transform.position.x, transform.position.y + startSpawnHeightOffset, transform.position.z);
+        Vector3 boxSize = new Vector3(width, startSpawnHeight, 1);
+        Gizmos.DrawWireCube(boxPos, boxSize);
     }
 }
